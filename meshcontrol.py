173576f75c7dee9a84022a9278455ef4af84ff2b
gabeladd <gabe@gabeladd.com>
2020-11-10 16:15:39 -0500

    added pritty print

--- meshcontrol.py
+++ meshcontrol.py
@@ -2,27 +2,61 @@ import meshtastic
 from pubsub import pub
 import datetime
 import time
-
-def onReceive(packet):  # called when a packet arrives
-    print(f"Received: {packet}")
+import json
 
 
-def onConnection():  # called when we (re)connect to the radio
+# def onReceive(packet, interface):  # called when a packet arrives
+#     print(f"Received: {packet}")
+
+
+# # called when we (re)connect to the radio
+# def onConnection(interface, topic=pub.AUTO_TOPIC):
+#     # defaults to broadcast, specify a destination ID if you wish
+#     interface.sendText("hello mesh")
+
+
+# pub.subscribe(onReceive, "meshtastic.receive")
+# pub.subscribe(onConnection, "meshtastic.connection.established")
+# # By default will try to find a meshtastic device, otherwise provide a device path like /dev/ttyUSB0
+# interface = meshtastic.SerialInterface()
+
+# bob = "Stop"
+
+# interface.connect()
+
+# # for n in range(1,10):
+# #     time.sleep(10)
+# #     interface.sendText(f"{n:2} | The current time is: {datetime.datetime.now()}")
+    
+
+# interface.sendText(f"Console complete. Self destruct engaged.")
+# interface.close()
+
+import meshtastic
+from pubsub import pub
+
+
+def onReceive(packet, interface):  # called when a packet arrives
+    print(f"\nReceived: {json.dumps(packet, indent=2)}")
+    #todo add write to file or SQS here
+    
+    bob = "STOP"
+
+
+# called when we (re)connect to the radio
+def onConnection(interface, topic=pub.AUTO_TOPIC):
     # defaults to broadcast, specify a destination ID if you wish
     interface.sendText("hello mesh")
+    # interface.sendText("Lily Go Go, Check for receipt.",
+    #                    destinationId='2988739000')
 
 
 pub.subscribe(onReceive, "meshtastic.receive")
 pub.subscribe(onConnection, "meshtastic.connection.established")
 # By default will try to find a meshtastic device, otherwise provide a device path like /dev/ttyUSB0
-interface = meshtastic.StreamInterface()
+interface = meshtastic.SerialInterface()
+# interface.setOwner(long_name = "Master Control Program", short_name="MCP")
+interface.setOwner("Master Control Program")
 
-# for n in range(1,10):
-#     time.sleep(10)
-#     interface.sendText(f"{n:2} | The current time is: {datetime.datetime.now()}")
-    
-
-interface.sendText(f"Console complete. Self destruct engaged.")
-
-print(f"--------- End of script! ---------")
+# print(f"--------- End of script! ---------")
 
